e163a846afd17dc01d47791f83e35f75c7935408
Noe Rivals <noe.rivals@epitech.eu>
2019-08-23 17:17:19 +0200

    Updates

---
+++ precache-manifest.7c41466548d63a488fdda2ca76299d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb01c673b8cb826fd46867b141e892df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbfe11da30c6a62918d",
+    "url": "/static/css/main.a958979a.chunk.css"
+  },
+  {
+    "revision": "f73af404d617bd9965a7",
+    "url": "/static/js/2.4037a066.chunk.js"
+  },
+  {
+    "revision": "ccbfe11da30c6a62918d",
+    "url": "/static/js/main.658959c7.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  }
+]);
