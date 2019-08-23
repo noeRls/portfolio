34ad200292e56ddf7505595f76fd141fb3ecf6e1
Noe Rivals <noe.rivals@epitech.eu>
2019-08-23 16:59:29 +0200

    [*](src/scenes/cmd): add cmd content

--- src/scenes/cmd/index.js
+++ src/scenes/cmd/index.js
@@ -6,10 +6,27 @@ class CMD extends React.Component {
     return (
       <div className="background">
         <div className="cmd-container cmd">
-          <div className="cmd-button-container">
-            <span className="cmd-button" style={{ backgroundColor: '#F6634F' }} />
-            <span className="cmd-button" style={{ backgroundColor: '#FFBB2F' }} />
-            <span className="cmd-button" style={{ backgroundColor: '#1CCC42' }} />
+          <div className="cmd-top-container">
+            <span className="box" />
+            <span className="box">
+              Noe Rivals
+            </span>
+            <span className="box">
+              <span className="cmd-button cmd-button-red" />
+              <span className="cmd-button cmd-button-orange" />
+              <span className="cmd-button cmd-button-green" />
+            </span>
+          </div>
+          <div className="cmd-content-container">
+            <div className="cmd-content cmd-content-first">
+              [noe@noe-pc ~]$
+            </div>
+            <div className="cmd-content">
+              2
+            </div>
+            <div className="cmd-content">
+              3
+            </div>
           </div>
         </div>
       </div>
