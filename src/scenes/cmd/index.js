c080585e0ad86dce9affee015d26aa9f0c481078
Noe Rivals <noe.rivals@epitech.eu>
2019-08-23 19:06:34 +0200

    [*](src/): start to work on input

--- src/scenes/cmd/index.js
+++ src/scenes/cmd/index.js
@@ -1,10 +1,25 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import './index.css';
+import AutosizeInput from 'react-input-autosize';
 
 class CMD extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: 'ls -la',
+    };
+  }
+
+  inputOnChange = e => this.setState({ input: e.target.value });
+
   render() {
+    const { input } = this.state;
     return (
-      <div className="background">
+      <div
+        className="background"
+      >
         <div className="cmd-container cmd">
           <div className="cmd-top-container">
             <span className="box" />
@@ -17,9 +32,20 @@ class CMD extends React.Component {
               <span className="cmd-button cmd-button-green" />
             </span>
           </div>
-          <div className="cmd-content-container">
+          <div
+            className="cmd-content-container"
+            onClick={() => this.inputRef.focus()}
+          >
             <div className="cmd-content cmd-content-first">
               [noe@noe-pc ~]$
+              <AutosizeInput
+                ref={ref => { this.inputRef = ref; }}
+                inputClassName="cmd-input"
+                className="cmd-input-container"
+                onChange={this.inputOnChange}
+                value={input}
+              />
+              <span className="input-carret" />
             </div>
             <div className="cmd-content">
               2
