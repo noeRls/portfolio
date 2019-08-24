d5470d21be72056bc32fad386d54203c1522332c
Noe Rivals <noe.rivals@epitech.eu>
2019-08-24 21:23:09 +0200

    Updates

---
+++ precache-manifest.cca50503b9624ee06487a9947840246b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dd758cefab1fa9e935b3f43b77516a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecc90dd8df53b9117b3f",
+    "url": "/static/css/main.5c99109a.chunk.css"
+  },
+  {
+    "revision": "1bb7db6d709624d03778",
+    "url": "/static/js/2.51497dd3.chunk.js"
+  },
+  {
+    "revision": "ecc90dd8df53b9117b3f",
+    "url": "/static/js/main.3e5e722b.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5519e861c3f4643c81002498e97720b2.js"
+  "/precache-manifest.cca50503b9624ee06487a9947840246b.js"
 );
 
 self.addEventListener('message', (event) => {
