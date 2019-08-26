272aad5506ba4e83d465d6168be9343eb361e2e5
Noe Rivals <noe.rivals@epitech.eu>
2019-08-26 16:21:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5b8db30c235d294467f5231068b14c3.js"
+  "/precache-manifest.9543088e1b070922c3ecdf56cf1ae761.js"
 );
 
 self.addEventListener('message', (event) => {
