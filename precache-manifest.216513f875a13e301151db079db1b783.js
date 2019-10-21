285e82c14dce1acfa23ed2793d1b0d9a01bda329
Noe Rivals <noe.rivals@epitech.eu>
2019-10-21 22:55:25 +0200

    Updates

---
+++ precache-manifest.216513f875a13e301151db079db1b783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b95fc95dbc4076ad3d21ab09798d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "954fa8118f36e548832f",
+    "url": "/static/css/main.b9b54e46.chunk.css"
+  },
+  {
+    "revision": "92161fbabc1954ef38bc",
+    "url": "/static/js/2.70c5cf89.chunk.js"
+  },
+  {
+    "revision": "954fa8118f36e548832f",
+    "url": "/static/js/main.44e52ea4.chunk.js"
+  },
+  {
+    "revision": "7c23a43b52e067a1c7c0",
+    "url": "/static/js/runtime~main.f6dce284.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fef5480c460d76406a7daca8795323c.js"
+  "/precache-manifest.216513f875a13e301151db079db1b783.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
